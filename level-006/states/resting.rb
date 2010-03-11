9a5493076e5c4e13d8178e104b48d5328978a788
Jrod <jarrod@jtms.net>
2010-03-11 13:02:45 -0500

    completes level 6

---
+++ level-006/states/resting.rb
@@ -0,0 +1,20 @@
+class Resting < State
+  def self.enter(player)
+    player.rest!
+  end
+
+  def self.exit(player)
+  end
+
+  def self.execute(player)
+    if player.under_attack?
+      player.current_state = Advancing
+    else
+      if !player.health_full?
+        player.rest!
+      else
+        player.current_state = Walking
+      end
+    end
+  end
+end
